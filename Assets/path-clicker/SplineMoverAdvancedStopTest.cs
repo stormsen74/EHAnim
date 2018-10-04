6eb0c9ea10261107629f32737ea2f81db2db1071
stormsen <micha@edit23.de>
2018-10-04 21:13:10 +0200

    Fix 'Jump' at Start after Stopping -> Spline

--- Assets/path-clicker/SplineMoverAdvancedStopTest.cs
+++ Assets/path-clicker/SplineMoverAdvancedStopTest.cs
@@ -39,43 +39,51 @@ public class SplineMoverAdvancedStopTest : MonoBehaviour {
 
     private Transform[] controlPoints;
 
-    private int stopStart = 0;
-    private int stopEnd = 0;
+    private bool hasStopped = false;
+    private int lastSegment = 0;
 
 
     // ------------------
 
-    void Start() {
+    void Start()
+    {
 
         //trail = transform.GetComponentInChildren<TrailRenderer>();
 
         controlPoints = new Transform[pointHolder.childCount];
-        for (int i = 0; i < controlPoints.Length; i++) {
+        for (int i = 0; i < controlPoints.Length; i++)
+        {
             controlPoints[i] = pointHolder.GetChild(i);
             //Debug.Log(controlPoints[i].transform.position.x);
         }
 
-        if (showControlPoints) {
-            for (int i = 0; i < controlPoints.Length; i++) {
+        if (showControlPoints)
+        {
+            for (int i = 0; i < controlPoints.Length; i++)
+            {
                 controlPoints[i].GetChild(0).localScale = new Vector3(.05f, .05f, .05f);
             }
-        } else {
-            for (int i = 0; i < controlPoints.Length; i++) {
+        }
+        else
+        {
+            for (int i = 0; i < controlPoints.Length; i++)
+            {
                 controlPoints[i].GetChild(0).localScale = new Vector3(.0f, .0f, .0f);
             }
         }
 
 
-        if (spline == null && controlPoints.Length > 2) {
+        if (spline == null && controlPoints.Length > 2)
+        {
             spline = new CatmullRom(controlPoints, resolution);
-            Debug.Log(spline.controlPoints[3] == spline.controlPoints[4]);
         }
 
     }
 
 
 
-    private void UpdatePosition(float time) {
+    private void UpdatePosition(float time)
+    {
 
         pointsLength = spline.GetPoints().Length - 2;
         timeStep = 1.0f / pointsLength;
@@ -86,7 +94,8 @@ public class SplineMoverAdvancedStopTest : MonoBehaviour {
         currentPathSegmentDisplay = currentPathSegment;
 
 
-        if (currentSegment < pointsLength + 1) {
+        if (currentSegment < pointsLength + 1)
+        {
 
             Vector3 posStart = spline.GetPoints()[currentSegment].position;
             Vector3 tanStart = spline.GetPoints()[currentSegment].tangent;
@@ -94,60 +103,27 @@ public class SplineMoverAdvancedStopTest : MonoBehaviour {
             Vector3 posEnd = spline.GetPoints()[currentSegment + 1].position;
             Vector3 tanEnd = spline.GetPoints()[currentSegment + 1].tangent;
 
-            //Quaternion qStart = Quaternion.LookRotation(tanStart);
-            //Quaternion qEnd = Quaternion.LookRotation(tanEnd);
-
-            //if(posStart == posEnd);
-
-            //Debug.Log(tanStart.magnitude);
-
-            //if (spline.controlPoints[currentPathSegment] == spline.controlPoints[currentPathSegment + 1]) {
-            //    Debug.Log(currentSegment);
-            //} else {
-            //    if (currentSegment == 29 + 1) {
-            //        transform.position = Vector3.Lerp(transform.position, posEnd, currentProgress);
-            //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(tanEnd), currentProgress);
-            //    } else {
-            //        transform.position = Vector3.Lerp(posStart, posEnd, currentProgress);
-            //        transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(tanStart), Quaternion.LookRotation(tanEnd), currentProgress);
-            //    }
-            //}
-
-            if (spline.controlPoints[currentPathSegment] == spline.controlPoints[currentPathSegment + 1]) {
-            } else {
-                transform.position = Vector3.Lerp(posStart, posEnd, currentProgress);
-                transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(tanStart), Quaternion.LookRotation(tanEnd), currentProgress);
+            if (spline.controlPoints[currentPathSegment] == spline.controlPoints[currentPathSegment + 1])
+            {
+                hasStopped = true;
+                lastSegment = currentSegment;
+            }
+            else
+            {
+                if (currentSegment <= lastSegment + 1 && hasStopped)
+                {
+                    transform.position = Vector3.Lerp(transform.position, posEnd, currentProgress * .1f);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(tanEnd), currentProgress * .2f);
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(posStart, posEnd, currentProgress);
+                    transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(tanStart), Quaternion.LookRotation(tanEnd), currentProgress);
+                    hasStopped = false;
+                }
             }
 
-
             //if (tanStart.magnitude != 0 && tanEnd.magnitude != 0) {
-
-
-            //} else {
-            //    // get last and first valid segment ...
-            //    if (stopStart == 0) stopStart = currentSegment;
-            //    if (stopEnd < currentSegment) stopEnd = currentSegment;
-            //}
-
-
-
-
-            //if (qEnd.eulerAngles.y != 0 && qStart.eulerAngles.y != 0) {
-            //    transform.rotation = Quaternion.Slerp(qStart, qEnd, currentProgress);
-            //    transform.position = Vector3.Lerp(posStart, posEnd, currentProgress);
-            //    if (currentSegment == stopEnd + 1) {
-            //        transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(spline.GetPoints()[stopStart].tangent), Quaternion.LookRotation(spline.GetPoints()[stopEnd + 1].tangent), currentProgress);
-            //    } else {
-            //        stopStart = 0;
-            //        stopEnd = 0;
-            //    }
-            //} else {
-            //    // get last and first valid segment ...
-            //    if (stopStart == 0) stopStart = currentSegment;
-            //    if (stopEnd < currentSegment) stopEnd = currentSegment;
-            //}
-
-
 
         }
 
@@ -155,20 +131,25 @@ public class SplineMoverAdvancedStopTest : MonoBehaviour {
     }
 
 
-    private float GetTimeScale() {
+    private float GetTimeScale()
+    {
         return 1 / duration;
     }
 
-    private float GetSpeed() {
+    private float GetSpeed()
+    {
         return vesselSpeed;
     }
 
-    private float UpdateProgress() {
+    private float UpdateProgress()
+    {
         return !useDuration ? GetSpeed() : GetTimeScale();
     }
 
-    void Update() {
-        if (spline != null) {
+    void Update()
+    {
+        if (spline != null)
+        {
             spline.Update(controlPoints);
             spline.Update(resolution);
             spline.DrawSpline(Color.white);
@@ -178,14 +159,19 @@ public class SplineMoverAdvancedStopTest : MonoBehaviour {
 
             if (drawTangent)
                 spline.DrawTangents(tangentExtrusion, Color.cyan);
-        } else {
+        }
+        else
+        {
             spline = new CatmullRom(controlPoints, resolution);
         }
 
 
-        if (globalTime <= 1) {
+        if (globalTime <= 1)
+        {
             globalTime += Time.deltaTime * UpdateProgress();
-        } else {
+        }
+        else
+        {
             globalTime = .001f;
         }
 
