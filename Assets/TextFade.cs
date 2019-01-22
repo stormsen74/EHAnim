bc1cc690e276a71966437d84b797c62396624dd6
Michael Sturm <mst@demodern.de>
2019-01-22 14:05:49 +0100

    Add more Tests

---
+++ Assets/TextFade.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using DG.Tweening;
+
+public class TextFade : MonoBehaviour {
+
+
+    [SerializeField]
+    private TextMeshProUGUI TMText;
+
+    Color textColor0 = new Color32(255, 255, 255, 0);
+    Color textColor1 = new Color32(255, 255, 255, 255);
+
+    void Start () {
+
+        TMText.color = textColor0;
+
+    }
+
+    private void FadeIn() {
+        TMText.DOColor(textColor1, 1).SetEase(Ease.InOutCubic);
+    }
+
+    private void FadeOut() {
+        TMText.DOColor(textColor0, 1).SetEase(Ease.InOutCubic);
+    }
+
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            FadeIn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            FadeOut();
+        }
+
+    }
+}
